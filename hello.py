51bf9cf350085f130718fd94cdd41143dd9601db
Nassila HAMOUCHE <nassilahamouche@gmail.com>
2021-04-01 15:48:08 +0200

    fix bug pylint

--- hello.py
+++ hello.py
@@ -1,14 +1,17 @@
+"""A dummy docstring."""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """A dummy docstring."""
     return "Hello world"
 
 @app.route('/hello/<name>')
 def hello(name=None):
-  return 'Hello ' + str(name)
+    """A dummy docstring."""
+    return 'Hello ' + str(name)
 
 if __name__ == "__main__":
     app.run()
